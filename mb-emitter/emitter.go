8f06b9184b8e2901c733d358cfcb3c6ecaeb4a81
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-05-28 01:17:55 +0200

    Modify the emitter to make it wieldy and stable
    We just need to work on deploying 'mock' versions for
    testing when no probes are available for reading. Other
    than that it's pretty much ready!

--- mb-emitter/emitter.go
+++ mb-emitter/emitter.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"log"
-	"os"
 	"time"
 	"ulbios/rfm9x-driver"
 
@@ -15,32 +13,7 @@ import (
 	"periph.io/x/host/v3"
 )
 
-type Data_point struct {
-	Msg  string `json:"msg"`
-	Data int    `json:"data"`
-}
-
-var param_to_addr map[string]uint16 = map[string]uint16{
-	"v_1": 0, "v_2": 1, "c_1": 2, "c_2": 3,
-}
-
-var (
-	poll_interval int
-	read_param    string
-
-	serial_dev string
-	parity     string
-	slave_id   int
-	baud_rate  int
-	data_bits  int
-	stop_bits  int
-	timeout    int
-
-	lora_enable   bool
-	lora_spi_port string
-)
-
-func get_mb_cli(serial_dev string) (modbus.Client, *modbus.RTUClientHandler) {
+func GetModBusCli(serial_dev string) (modbus.Client, *modbus.RTUClientHandler) {
 	handler := modbus.NewRTUClientHandler(serial_dev)
 
 	handler.Parity = parity
@@ -58,7 +31,7 @@ func get_mb_cli(serial_dev string) (modbus.Client, *modbus.RTUClientHandler) {
 	return modbus.NewClient(handler), handler
 }
 
-func read_420(c modbus.Client) (uint32, error) {
+func Read420(c modbus.Client) (uint32, error) {
 	r_data, err := c.ReadHoldingRegisters(param_to_addr[read_param], 1)
 	if err != nil {
 		return 0, err
@@ -66,7 +39,7 @@ func read_420(c modbus.Client) (uint32, error) {
 	return uint32(r_data[0])<<8 | uint32(r_data[1]), nil
 }
 
-func get_lora_cli() (*rfm9x.Dev, spi.PortCloser) {
+func GetLoRaCli() (*rfm9x.Dev, spi.PortCloser) {
 	if _, err := host.Init(); err != nil {
 		log.Printf("error initialising Periph: %v\n", err)
 		return nil, nil
@@ -88,56 +61,19 @@ func get_lora_cli() (*rfm9x.Dev, spi.PortCloser) {
 		log.Fatalf("error instantiating the LoRa radio: %v\n", err)
 		return nil, nil
 	}
-
 	return radio, p
 }
 
-func send_over_lora(r *rfm9x.Dev, msg string, data int) error {
-	enc_payload, err := json.Marshal(Data_point{Msg: msg, Data: data})
+func SendOverLoRa(r *rfm9x.Dev, id string, data int) error {
+	dp := DataPoint{Id: id, Data: data}
+
+	log.Printf("sending %#v", dp)
+
+	enc_payload, err := json.Marshal(dp)
 	if err != nil {
 		log.Printf("error marshalling data: %v\n", err)
 		return err
 	}
+
 	return r.Send(enc_payload)
-}
-
-func main() {
-	// General stuff
-	flag.IntVar(&poll_interval, "poll-interval", 30, "Poll interval in seconds")
-	flag.StringVar(&read_param, "read-param", "c-1", "Parameter to read over ModBus")
-
-	// Serial stuff
-	flag.StringVar(&serial_dev, "serial-device", "/dev/ttyUSB0", "Serial device to listen on: a path to a device or 'NONE'")
-	flag.StringVar(&parity, "serial-parity", "N", "Serial parity: N | E | O")
-	flag.IntVar(&slave_id, "slave-id", 1, "Slave ID to connect to [1, 247]")
-	flag.IntVar(&baud_rate, "baud-rate", 115200, "Server baud rate in bps")
-	flag.IntVar(&data_bits, "data-bits", 8, "Data bits")
-	flag.IntVar(&stop_bits, "stop-bits", 1, "Stop bits")
-	flag.IntVar(&timeout, "timeout", 10, "Timeout in s")
-
-	// LoRa stuff
-	flag.BoolVar(&lora_enable, "lora-enable", false, "Whether to enable data reception over LoRa")
-	flag.StringVar(&lora_spi_port, "lora-spi-port", "/dev/spidev0.1", "SPI address the radio is on")
-
-	flag.Parse()
-
-	// Remove leading date and time from log messages
-	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
-
-	mb_cli, mb_handler := get_mb_cli(serial_dev)
-	defer mb_handler.Close()
-
-	lora_cli, lora_pc := get_lora_cli()
-	defer lora_pc.Close()
-
-	for range time.Tick(time.Second * time.Duration(poll_interval)) {
-		data, err := read_420(mb_cli)
-		if err != nil {
-			log.Printf("error reading 420 data: %v\n", err)
-			continue
-		}
-		if err := send_over_lora(lora_cli, func() string { hn, _ := os.Hostname(); return hn }(), int(data)); err != nil {
-			log.Printf("error sending data over LoRa: %v\n", err)
-		}
-	}
 }
---
+++ mb-emitter/root.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/go-co-op/gocron"
+)
+
+type DataPoint struct {
+	Id   string `json:"id"`
+	Data int    `json:"data"`
+}
+
+var (
+	poll_interval string
+	read_param    string
+
+	serial_dev string
+	parity     string
+	slave_id   int
+	baud_rate  int
+	data_bits  int
+	stop_bits  int
+	timeout    int
+
+	lora_enable   bool
+	lora_spi_port string
+
+	param_to_addr map[string]uint16 = map[string]uint16{
+		"v_1": 0, "v_2": 1, "c_1": 2, "c_2": 3,
+	}
+
+	rootCmd = &cobra.Command{
+		Use:   "mb-emitter",
+		Short: "A ModBus Emitter reading 4-20 mA data and sending it over LoRa.",
+		Long: "This executable implements a ModBus client over RTU-over-serial that queries a 4-20 -- ModBus transducer.\n" +
+			"Retrieved data is then sent over LoRa so that it can be acquired by a main gateway.\n",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 0 {
+				return fmt.Errorf("no arguments should be provided (configuration is done through flags)")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			// Remove leading date and time from log messages
+			log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
+			// mb_cli, mb_handler := GetModBusCli(serial_dev)
+			// defer mb_handler.Close()
+
+			lora_cli, lora_pc := GetLoRaCli()
+			defer lora_pc.Close()
+
+			hn, _ := os.Hostname()
+
+			s := gocron.NewScheduler(time.UTC)
+
+			_, _ = s.CronWithSeconds(poll_interval).Do(func() {
+				// data, err := Read420(mb_cli)
+				// if err != nil {
+				// 	log.Printf("error reading 420 data: %v\n", err)
+				// 	return
+				// }
+				data := 10
+				if err := SendOverLoRa(lora_cli, hn, int(data)); err != nil {
+					log.Printf("error sending data over LoRa: %v\n", err)
+				}
+			})
+			s.StartBlocking()
+		},
+	}
+)
+
+func init() {
+	// Disable Cobra completions
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+
+	// General
+	// rootCmd.Flags().IntVar(&poll_interval, "poll-interval", 30, "Poll interval in seconds")
+
+	// The Cron syntax follows https://en.wikipedia.org/wiki/Cron
+	rootCmd.Flags().StringVar(&poll_interval, "poll-interval", "0 * * * * *",
+		"Poll interval as a Cron expression [1 minute @ minute start by default]")
+	rootCmd.Flags().StringVar(&read_param, "read-param", "c-1", "Parameter to read over ModBus")
+
+	// ModBus over RTU/Serial
+	rootCmd.Flags().StringVar(&serial_dev, "serial-device", "/dev/ttyUSB0", "Serial device to listen on: a path to a device or 'NONE'")
+	rootCmd.Flags().StringVar(&parity, "serial-parity", "N", "Serial parity: N | E | O")
+	rootCmd.Flags().IntVar(&slave_id, "slave-id", 1, "Slave ID to connect to [1, 247]")
+	rootCmd.Flags().IntVar(&baud_rate, "baud-rate", 115200, "Server baud rate in bps")
+	rootCmd.Flags().IntVar(&data_bits, "data-bits", 8, "Data bits")
+	rootCmd.Flags().IntVar(&stop_bits, "stop-bits", 1, "Stop bits")
+	rootCmd.Flags().IntVar(&timeout, "timeout", 10, "Timeout in s")
+
+	// Data output over LoRa
+	rootCmd.Flags().BoolVar(&lora_enable, "lora-enable", false, "Whether to enable data reception over LoRa")
+	rootCmd.Flags().StringVar(&lora_spi_port, "lora-spi-port", "/dev/spidev0.1", "SPI address the radio is on")
+}
