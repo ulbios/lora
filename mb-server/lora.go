060055fa955008a121b534e643a2ab4b99c7df20
Pablo Collado Soto <pcolladosoto@gmx.com>
2023-02-27 18:17:49 +0100

    Make the LoRa receiver sturdier

--- mb-server/lora.go
+++ mb-server/lora.go
@@ -4,12 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 	"ulbios/rfm9x-driver"
 
 	mbclient "github.com/goburrow/modbus"
 	"periph.io/x/conn/v3/spi/spireg"
 	"periph.io/x/host/v3"
 )
+
+var lora_debug = []rfm9x.Log_level{
+	rfm9x.LogLevelErr,
+	rfm9x.LogLevelWarn,
+	rfm9x.LogLevelInfo,
+	rfm9x.LogLevelDebug,
+	rfm9x.LogLevelRegIO,
+}
 
 func InsertDataLoRa(freq int64) error {
 	handler := mbclient.NewTCPClientHandler(fmt.Sprintf("%s:%d", mb_bind_addr, mb_bind_port))
@@ -40,6 +49,7 @@ func InsertDataLoRa(freq int64) error {
 
 	d_opts := rfm9x.DefaultOpts
 	d_opts.FrequencyMHz = freq
+	d_opts.LogLevel = lora_debug[lora_debug_level]
 
 	radio, err := rfm9x.New(
 		p,
@@ -52,9 +62,14 @@ func InsertDataLoRa(freq int64) error {
 	var dp DataPoint
 
 	for {
-		enc_pkt, err := radio.Receive(0)
+		enc_pkt, err := radio.Receive(
+			time.Duration(lora_recv_wait)*time.Millisecond, time.Duration(lora_recv_timeout)*time.Millisecond)
 		if err != nil {
 			log.Printf("LoRa: error receiving data: %v\n", err)
+			continue
+		}
+		if len(enc_pkt) < 5 {
+			log.Printf("LoRa: the received packet is too short: %v (len %d)\n", enc_pkt, len(enc_pkt))
 			continue
 		}
 		if err := json.Unmarshal(enc_pkt[4:], &dp); err != nil {
@@ -67,6 +82,11 @@ func InsertDataLoRa(freq int64) error {
 		addr, ok := id_to_mb_addr[dp.Id]
 		if !ok {
 			log.Printf("LoRa: received a nonexistent ID: %s\n", dp.Id)
+			continue
+		}
+
+		if dp.Data == 0 {
+			log.Printf("LoRa: ignoring incorrect data value %d and sticking to the last one\n", dp.Data)
 			continue
 		}
 
